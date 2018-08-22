f109858b4b2713f07d6dfddf7cda8df761054069
Stoyanov <dimo.stoyanov@dxc.com>
2018-08-22 16:20:07 +0300

    Add project files.

---
+++ ExpenseManager/Migrations/20180822121356_minnorChangesAdded.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpenseManager.Migrations
+{
+    public partial class minnorChangesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ItemName",
+                table: "ExpenseReport",
+                newName: "Item name");
+
+            migrationBuilder.RenameColumn(
+                name: "ExpenseDate",
+                table: "ExpenseReport",
+                newName: "Expense date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Item name",
+                table: "ExpenseReport",
+                newName: "ItemName");
+
+            migrationBuilder.RenameColumn(
+                name: "Expense date",
+                table: "ExpenseReport",
+                newName: "ExpenseDate");
+        }
+    }
+}
---
+++ ExpenseManager/Models/ExpenseDBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseManager.Models
+{
+    public class ExpenseDBContext : DbContext
+    {
+        public ExpenseDBContext()
+        {
+        }
+
+        public ExpenseDBContext(DbContextOptions<ExpenseDBContext> options)
+            :base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ExpenseReportDB;Integrated Security=True;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+
+
+        public virtual DbSet<ExpenseReport> ExpenseReport { get; set; }
+    }
+}
